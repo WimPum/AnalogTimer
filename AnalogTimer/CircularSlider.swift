c3f64347c8de82ceba35716e66ce12a3224fd6db
WimPum <n1v0x.5127e@gmail.com>
2024-04-25 20:08:53 +0900

    CircularSlider (this will be clockhands)

---
+++ AnalogTimer/CircularSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CircularSlider.swift
+//  AnalogTimer
+//
+//  Created by 虎澤謙 on 2024/04/20.
+//
+
+import SwiftUI
+
+struct CircularSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularSlider()
+}
