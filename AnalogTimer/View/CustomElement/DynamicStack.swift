8b6bd2fea561b148ae6f4d8564f0b64c53017e7a
WimPum <n1v0x.5127e@gmail.com>
2024-11-06 17:11:38 +0900

    DynamicStack

---
+++ AnalogTimer/View/CustomElement/DynamicStack.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicStack.swift
+//  AnalogTimer
+//
+//  Created by 虎澤謙 on 2024/11/06.
+//
+
+import SwiftUI
+
+struct DynamicStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DynamicStack()
+}
